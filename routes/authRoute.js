007a7157c3d8794670d27cfd5f33874814aac410
yellowhatpro <yellowhatpro3119@gmail.com>
2024-03-22 08:01:45 +0530

    some restructuring

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,27 +1,31 @@
-import express from 'express'
-import { registerController, loginController, forgotPasswordController } from '../controllers/authController.js'
-import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js'
+import express from "express";
+import {
+  registerController,
+  loginController,
+  forgotPasswordController,
+} from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
 //router object
-const router = express.Router()
+const router = express.Router();
 
 //routing
 //REGISTER || METHOD POST
-router.post('/register', registerController)
+router.post("/register", registerController);
 
-//LOGIN || POST 
-router.post('/login', loginController)
+//LOGIN || POST
+router.post("/login", loginController);
 
 // protected  User route auth
-router.get('/user-auth', requireSignIn, (req, res) => {
-  res.status(200).send({ ok: true })
-})
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
 
 // protected Admin route auth
-router.get('/admin-auth', requireSignIn, isAdmin, (req, res) => {
-  res.status(200).send({ ok: true })
-})
+router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
+  res.status(200).send({ ok: true });
+});
 
-router.post('/forgot-password', forgotPasswordController)
+router.post("/forgot-password", forgotPasswordController);
 
-export default router
+export default router;
